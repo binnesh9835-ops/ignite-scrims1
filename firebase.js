070f9082ae7aafa13e9af7dcac6ae0120d2af2e0
binnesh9835-ops <binnesh9835@gmail.com>
2026-03-15 01:52:23 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,42 +1,20 @@
-// FIREBASE IMPORT
+// firebase.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-
-
-// FIREBASE CONFIG
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyCgyT_wRam-8FWkq5VePffFtymUMbRnXCQ",
-
   authDomain: "ignite-scrims.firebaseapp.com",
-
   projectId: "ignite-scrims",
-
   storageBucket: "ignite-scrims.firebasestorage.app",
-
   messagingSenderId: "497561769270",
-
   appId: "1:497561769270:web:ef4f215a253e984f2dcf97"
 };
 
-
-
-// INITIALIZE FIREBASE
-
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
+export const auth = getAuth(app);
 
-const db = getFirestore(app);
-
-
-
-// EXPORT
-
-export { auth, db };
+export const provider = new GoogleAuthProvider();
