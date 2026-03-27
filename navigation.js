9118521a0daf21e0677ead58a7383b02e01826b1
binnesh9835-ops <binnesh9835@gmail.com>
2026-03-28 02:14:07 +0530

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,1 +1,27 @@
+let popupStack = [];
 
+/* OPEN POPUP */
+function openPopup(id){
+  let popup = document.getElementById(id);
+  if(popup){
+    popup.style.display = "flex";
+    popupStack.push(id);
+  }
+}
+
+/* CLOSE POPUP */
+function closePopup(id){
+  let popup = document.getElementById(id);
+  if(popup){
+    popup.style.display = "none";
+    popupStack = popupStack.filter(p => p !== id);
+  }
+}
+
+/* GO BACK */
+function goBack(){
+  if(popupStack.length > 0){
+    let last = popupStack.pop();
+    document.getElementById(last).style.display = "none";
+  }
+}
