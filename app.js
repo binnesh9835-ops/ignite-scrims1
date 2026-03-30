9741c9f2ba592af8001c6b156b954c6c2db8da75
binnesh9835-ops <binnesh9835@gmail.com>
2026-03-30 19:14:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+function googleLogin() {
+    auth.signInWithPopup(provider)
+    .then((result) => {
+        let isNewUser = result.additionalUserInfo.isNewUser;
 
+        if (isNewUser) {
+            document.getElementById("welcomePopup").classList.remove("hidden");
+        } else {
+            window.location.href = "dashboard.html";
+        }
+    })
+    .catch((error) => {
+        alert(error.message);
+    });
+}
+
+function closePopup() {
+    window.location.href = "dashboard.html";
+}
